8e4d6f20f01135ac9ba37c01154395cd5f7bf956
Carlos Monzalvo N <carlos.monzalvo.n@gmail.com>
2019-10-04 20:10:52 -0500

    Modificacion de Credentials y Cliente

--- Fakestagram-Xcode10/Fakestagram-Xcode10/Credentials.swift
+++ Fakestagram-Xcode10/Fakestagram-Xcode10/Credentials.swift
@@ -7,3 +7,18 @@
 //
 
 import Foundation
+
+enum Credentials{
+    case apiToken
+    
+    func get()-> String?{
+        switch self {
+        case .apiToken:
+            return "f41af9b1-5a7e-4f0b-8c88-e44f686b1d2e"
+        }
+    }
+    
+    func set(value: String) ->Bool{
+        return true;
+    }
+}
--- Fakestagram-Xcode10/Fakestagram-Xcode10/DataSource/Cliente.swift
+++ Fakestagram-Xcode10/Fakestagram-Xcode10/DataSource/Cliente.swift
@@ -7,3 +7,58 @@
 //
 
 import Foundation
+
+class Cliente{
+    let session: URLSession
+    let baseUrl : URL
+    let contentType: String
+    
+    init(session: URLSession, baseUrl: URL, contentType: String) {
+        self.session = session
+        self.baseUrl = baseUrl
+        self.contentType = contentType
+    }
+    
+    typealias successfulResponse = (Data) -> Void
+    
+    func get(path: String, success: successfulResponse){}
+    
+    func post(path: String,body: Data?, success: successfulResponse){}
+    
+    func put (path: String,body: Data?, success: successfulResponse){}
+    
+    func delete(path: String, success: successfulResponse){}
+    
+    func request(method: String, path: String,body: Data?,success: successfulResponse) {
+        
+        let req = buildRequest(method: method, path: path,body: body)
+        
+        session.dataTask(with: req){ (data, response,error) in
+            if let error = error {
+                debugPrint(error)
+                return
+            }
+            let res = ClientResponse(response)
+            if res.successful? {
+                
+            }
+            }.resume()
+        
+    }
+    
+    private func buildRequest(method: String, path: String,body : Data?) -> URLRequest{
+        var urlComponents = URLComponents(url: self.baseUrl, resolvingAgainstBaseURL: true)!
+        urlComponents.path = path
+        var request = URLRequest(url: urlComponents.url!)
+        request.setValue(contentType, forHTTPHeaderField: "Accept")
+        request.setValue(contentType, forHTTPHeaderField: "Content-Type")
+        request.httpMethod = method
+        
+        if let token = Credentials.apiToken.get() {
+            request.addValue("Bearer \(token)", forHTTPHeaderField:"Authorization")
+        }
+        request.httpBody = body
+        
+        return request
+    }
+}
