55f061fe484554e7d5c55f8f6b119d9225c1c3b6
Luis Ezcurdia Razo <ing.ezcurdia@gmail.com>
2019-10-11 17:54:46 -0500

    RestClient basic implementations

---
+++ fakestagram/Models/Post.swift
@@ -0,0 +1,38 @@
+//
+//  Post.swift
+//  fakestagram
+//
+//  Created by LuisE on 10/5/19.
+//  Copyright © 2019 3zcurdia. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+struct Post: Codable {
+    let id: Int?
+    let title: String
+    let imageUrl: String?
+    var likesCount: Int
+    var commentsCount: Int
+    let createdAt: String
+    var liked: Bool
+    let location: String
+
+    func likesCountText() -> String {
+        return "\(likesCount) likes"
+    }
+
+    func commentsCountText() -> String {
+        return "\(commentsCount) comments"
+    }
+
+    func load(_ image: @escaping (UIImage) -> Void) {
+        guard let urlString = imageUrl, let url = URL(string: urlString) else { return }
+        DispatchQueue.global(qos: .background).async {
+            if let data = try? Data(contentsOf: url), let img = UIImage(data: data) {
+                DispatchQueue.main.async { image(img) }
+            }
+        }
+    }
+}
--- fakestagram/PostsTableViewController.swift
+++ fakestagram/PostsTableViewController.swift
@@ -8,34 +8,6 @@
 
 import UIKit
 
-struct Post: Codable {
-    let id: Int?
-    let title: String
-    let imageUrl: String?
-    var likesCount: Int
-    var commentsCount: Int
-    let createdAt: String
-    var liked: Bool
-    let location: String
-
-    func likesCountText() -> String {
-        return "\(likesCount) likes"
-    }
-
-    func commentsCountText() -> String {
-        return "\(commentsCount) comments"
-    }
-
-    func load(_ image: @escaping (UIImage) -> Void) {
-        guard let urlString = imageUrl, let url = URL(string: urlString) else { return }
-        DispatchQueue.global(qos: .background).async {
-            if let data = try? Data(contentsOf: url), let img = UIImage(data: data) {
-                DispatchQueue.main.async { image(img) }
-            }
-        }
-    }
-}
-
 class PostsTableViewController: UITableViewController {
     static let cellId = "PostCell"
     var posts = [Post]()
