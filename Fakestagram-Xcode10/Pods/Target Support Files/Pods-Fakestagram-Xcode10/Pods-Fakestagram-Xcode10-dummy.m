0398c9bd7481af82d584c90b4ce02a9c60ec7cce
Carlos Monzalvo N <carlos.monzalvo.n@gmail.com>
2019-10-11 19:55:29 -0500

    Add DVR with cocoapods

---
+++ Fakestagram-Xcode10/Pods/Target Support Files/Pods-Fakestagram-Xcode10/Pods-Fakestagram-Xcode10-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Fakestagram_Xcode10 : NSObject
+@end
+@implementation PodsDummy_Pods_Fakestagram_Xcode10
+@end
---
+++ Fakestagram-Xcode10/Pods/Target Support Files/Pods-Fakestagram-Xcode10Tests/Pods-Fakestagram-Xcode10Tests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Fakestagram_Xcode10Tests : NSObject
+@end
+@implementation PodsDummy_Pods_Fakestagram_Xcode10Tests
+@end
