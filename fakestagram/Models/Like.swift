585b87bbc23b6ddfef51e58f7ade0711b68704c5
Luis Ezcurdia Razo <ing.ezcurdia@gmail.com>
2019-10-12 14:03:44 -0500

    Implement views with XIB and programatic views

---
+++ fakestagram/Models/Like.swift
@@ -0,0 +1,13 @@
+//
+//  Like.swift
+//  fakestagram
+//
+//  Created by LuisE on 10/12/19.
+//  Copyright © 2019 3zcurdia. All rights reserved.
+//
+
+import Foundation
+
+struct Like: Codable {
+    let author: Author
+}
