fb29e46a578181ecd308237d44cd7330834c95a5
Luis Ezcurdia Razo <ing.ezcurdia@gmail.com>
2019-10-18 20:41:48 -0500

    Replace TableViewController with CollectionView through delegates

---
+++ fakestagram/Views/PostCollectionViewCell.swift
@@ -0,0 +1,42 @@
+//
+//  PostCollectionViewCell.swift
+//  fakestagram
+//
+//  Created by LuisE on 10/18/19.
+//  Copyright © 2019 3zcurdia. All rights reserved.
+//
+
+import UIKit
+
+class PostCollectionViewCell: UICollectionViewCell {
+    static let identifier: String = "PostCell"
+    public var post: Post? {
+        didSet {
+            updateView()
+        }
+    }
+    @IBOutlet weak var authorView: AuthorView!
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var likeCounter: UILabel!
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var likeBttn: UIButton!
+    @IBOutlet weak var commentBttn: UIButton!
+
+    func reset() {
+        self.imageView.image = nil
+        self.likeCounter.text = ""
+        self.titleLabel.text = ""
+    }
+
+    func updateView() {
+        reset()
+        guard let post = self.post else { return }
+        self.authorView.author = post.author
+        self.titleLabel.text = post.title
+        self.likeCounter.text = post.likesCountText()
+        post.load { [unowned self] img in
+            self.imageView.image = img
+        }
+    }
+
+}
