a2029c85d95c488e1cf6373925d6c8ec134ae617
Carlos Monzalvo N <carlos.monzalvo.n@gmail.com>
2019-11-09 11:17:39 -0600

    added post service

--- Fakestagram-Xcode10/Fakestagram-Xcode10/ViewControllers/CameraViewController.swift
+++ Fakestagram-Xcode10/Fakestagram-Xcode10/ViewControllers/CameraViewController.swift
@@ -7,18 +7,54 @@
 //
 
 import UIKit
+import CoreLocation
 
-class CameraViewController: UIViewController {
+class CameraViewController: UIViewController, CLLocationManagerDelegate {
 
-    let client = RestClient<CreatePost>(client: Client.fakestagram, basePath: "/api/v1/posts")
+    let locationManager = CLLocationManager()
+    let service = CreatePostService()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        enableBasicLocationServices()
+    }
+
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        locationManager.startUpdatingLocation()
+    }
+
+    override func viewWillDisappear(_ animated: Bool) {
+        locationManager.stopUpdatingLocation()
+        super.viewWillDisappear(animated)
+
+    }
 
     @IBAction func onTapCreate(_ sender: UIButton) {
         let img = UIImage(named: "pomememe")!
-        let newPost = CreatePost(title: "Karate Pome", imageData: img.base64())
-        client.create(newPost) { post in
+        service.call(image: img, title: "Karate Pome") { (postId) in
             print("Successful!")
-            print(post?.title)
-
+            print(postId ?? -1)
         }
+    }
+
+    func enableBasicLocationServices() {
+        locationManager.delegate = self
+        switch CLLocationManager.authorizationStatus() {
+        case .notDetermined:
+            locationManager.requestWhenInUseAuthorization()
+        case .restricted, .denied:
+            print("Disable location features")
+        case .authorizedWhenInUse, .authorizedAlways:
+            print("Enable location features")
+        @unknown default:
+            fatalError()
+        }
+    }
+
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.last else { return }
+        print(location)
+        service.update(coordinate: location.coordinate)
     }
 }
