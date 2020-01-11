ece48458c3619295655ecf9579c6468a2e5e711d
JavierFo <pk.gs.xv@gmail.com>
2020-01-10 19:08:36 -0600

    ...

--- fakestagram/Services/LikeService.swift
+++ fakestagram/Services/LikeService.swift
@@ -8,32 +8,6 @@
 
 import Foundation
 
-//class CommentsService {
-//
-//    func getComments(postId: Int, success: @escaping ([Comment]?) -> Void) {
-//        let client = RestClient<[Comment]>(client: Client.fakestagram, basePath: "/api/v1/posts/\(postId)/comments")
-//        client.show(success: success)
-//    }
-//}
-//
-//struct CreateLike: Codable {
-//    let postID: Int?
-//}
-//
-//class CreateLikeService {
-//    func call(postID: Int?, success: @escaping (Int?) -> Void) {
-//        let newLike = CreateLike(
-//            postID: postID
-//        )
-//
-//        let client = RestClient<CreateLike>(client: Client.fakestagram, basePath: "/api/v1/posts/\(newLike.postID!)/like")
-//        client.create(newLike) { post in
-//            NotificationCenter.default.post(name: NotificationKeys.didFinishPostCreation.value, object: nil)
-//            success(post?.postID)
-//        }
-//    }
-//}
-
 class LikeService {
     func getLikes(postID: Int?, success: @escaping (Int?) -> Void) {
         let client = RestClient<CreateLike>(client: Client.fakestagram, basePath: "/api/v1/posts/\(newLike.postID!)/like")
