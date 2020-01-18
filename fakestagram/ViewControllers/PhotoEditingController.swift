657f6cb65d5b259051c41c2b1d92d676ae37ead2
JavierFo <pk.gs.xv@gmail.com>
2020-01-18 06:45:28 -0600

    Swap PhotoEditView .Xib for .Storyboard

--- fakestagram/ViewControllers/PhotoEditingController.swift
+++ fakestagram/ViewControllers/PhotoEditingController.swift
@@ -13,16 +13,15 @@ import CoreImage
 class PhotoEditingViewController : UIViewController {
 
     @IBOutlet weak var imgview: UIImageView!
-    
+
   //  var imgview: UIImageView!
     private var originalImage : UIImage?
-    
-    
+
     struct Filtro {
         let nombreDeFiltro : String
         var valorFiltro : Any?
         var nombreDeValorDelFiltro: String?
-        
+
         init(nombreDeFiltro : String, valorFiltro : Any?, nombreDeValorDelFiltro: String?) {
             self.nombreDeFiltro = nombreDeFiltro
             self.nombreDeValorDelFiltro = nombreDeValorDelFiltro
@@ -31,64 +30,64 @@ class PhotoEditingViewController : UIViewController {
     }
     
     private func applyFilter(image : UIImage, filterEffect : Filtro) -> UIImage? {
-        
+
         guard let cgImage = image.cgImage else { return nil }
-        
+
         let context = CIContext(options: nil)
-        
+
         let ciImage = CIImage(cgImage: cgImage)
         let filter = CIFilter(name: filterEffect.nombreDeFiltro)
-        
+
         filter?.setValue(ciImage, forKey: kCIInputImageKey)
-        
+
         if let filterEffectValue = filterEffect.valorFiltro, let filterEffectValueName = filterEffect.nombreDeValorDelFiltro {
             filter?.setValue(filterEffectValue, forKey: filterEffectValueName)
         }
-        
+
         var imagenFiltrada : UIImage?
-        
+
         if let output = filter?.value(forKey: kCIOutputImageKey) as? CIImage, let cgiImageResult = context.createCGImage(output, from: output.extent){
             imagenFiltrada = UIImage(cgImage: cgiImageResult)
         }
         return imagenFiltrada
     }
-    
+
     @IBAction func applySepia(_ sender: Any){
-        
+
         guard let image = imgview.image else { return }
-        
+
         imgview.image = applyFilter(image: image, filterEffect: Filtro(nombreDeFiltro: "CISepiaTone", valorFiltro: 8.0, nombreDeValorDelFiltro: kCIInputIntensityKey))
-        
+
     }
-    
+
     @IBAction func applyPhotoTransferEffect(_ sender: Any){
-        
+
         guard let image = imgview.image else { return }
-        
+
         imgview.image = applyFilter(image: image, filterEffect: Filtro(nombreDeFiltro: "CIPhotoEffectProcess", valorFiltro: nil, nombreDeValorDelFiltro: nil))
-        
+
     }
-    
+
     @IBAction func applyBlur(_ sender: Any){
-        
+
         guard let image = imgview.image else { return }
-        
+
         imgview.image = applyFilter(image: image, filterEffect: Filtro(nombreDeFiltro: "CIGaussianBlur", valorFiltro: 8.0, nombreDeValorDelFiltro: kCIInputRadiusKey))
-        
+
     }
-    
+
     @IBAction func applyPhotoEffectNoir(_ sender: Any){
-        
+
         guard let image = imgview.image else { return }
-        
+
         imgview.image = applyFilter(image: image, filterEffect: Filtro(nombreDeFiltro: "CIPhotoEffectNoir", valorFiltro: nil, nombreDeValorDelFiltro: nil))
-        
+
     }
-    
+
     @IBAction func clearFilters(_ sender: Any){
-        
+
         imgview.image = originalImage
-        
+
     }
     
 }
